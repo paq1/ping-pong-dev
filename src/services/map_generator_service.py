ccfe20706b4b1573b94e742cb336d7a35205b128
Pierre Paquin <p.paquin1016@laposte.net>
2022-08-14 10:25:53 +0200

    refactor: simplification de la methode de génération de map aleatoire

--- src/services/map_generator_service.py
+++ src/services/map_generator_service.py
@@ -9,16 +9,13 @@ class MapGeneratorService:
     def generator_random_map(
         sizetilemap: tuple[int, int] = (10, 10)
     ) -> Tilemap:
-        
-        return Tilemap(id="random_map_test", tiles = [])
+        return Tilemap(id="random_map_test", tiles = MapGeneratorService.generate_tile_matrix_enum((10, 10)))
     
+    @staticmethod
     def generate_tile_matrix_enum(size_tilemap: tuple[int, int]) -> list[list[TileEnum]]:
-        tilemap = []
-        for i in range(size_tilemap[0]):
-            line = [random.randint(1,2) for j in range(size_tilemap[1])]
-            tilemap.append(line)
-        return tilemap
+        rows, cols = size_tilemap
+        return [[MapGeneratorService.get_rand_tile()] * cols] * rows
 
-    def ggg(size_tilemap: tuple[int, int]) -> list[list[TileEnum]]:
-        tilemap = [0 for i in range(size_tilemap[0])]
-        
+    @staticmethod
+    def get_rand_tile() -> int:
+        random.randint(1,2)
