5ceadcfdbf6895fb823e14111a10c0d63482ff9c
paq1 <32450977+paq1@users.noreply.github.com>
2022-08-14 10:26:59 +0200

    Poubelle (#2)
    * error: need fix and clean code
    
    * refactor: simplification de la methode de génération de map aleatoire

--- src/services/map_generator_service.py
+++ src/services/map_generator_service.py
@@ -1,9 +1,21 @@
 from models import Tilemap
+from enums import TileEnum
+import random
 
 
 class MapGeneratorService:
 
     @staticmethod
-    def generator_random_map() -> Tilemap:
-        return Tilemap(id="random_map_test", tiles = [])
+    def generator_random_map(
+        sizetilemap: tuple[int, int] = (10, 10)
+    ) -> Tilemap:
+        return Tilemap(id="random_map_test", tiles = MapGeneratorService.generate_tile_matrix_enum((10, 10)))
     
+    @staticmethod
+    def generate_tile_matrix_enum(size_tilemap: tuple[int, int]) -> list[list[TileEnum]]:
+        rows, cols = size_tilemap
+        return [[MapGeneratorService.get_rand_tile()] * cols] * rows
+
+    @staticmethod
+    def get_rand_tile() -> int:
+        random.randint(1,2)
