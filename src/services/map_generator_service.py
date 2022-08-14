2e2c6eff5e6311d5d4d2d2cc59704f533ddace26
Pierre Paquin <p.paquin1016@laposte.net>
2022-08-14 10:32:14 +0200

    fix: utilisation du parametre au lieu du code en dur

--- src/services/map_generator_service.py
+++ src/services/map_generator_service.py
@@ -9,7 +9,7 @@ class MapGeneratorService:
     def generator_random_map(
         sizetilemap: tuple[int, int] = (10, 10)
     ) -> Tilemap:
-        return Tilemap(id="random_map_test", tiles = MapGeneratorService.generate_tile_matrix_enum((10, 10)))
+        return Tilemap(id="random_map_test", tiles = MapGeneratorService.generate_tile_matrix_enum(sizetilemap))
     
     @staticmethod
     def generate_tile_matrix_enum(size_tilemap: tuple[int, int]) -> list[list[TileEnum]]:
